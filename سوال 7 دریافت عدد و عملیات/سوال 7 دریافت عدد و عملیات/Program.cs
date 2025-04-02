0e30e45a58b818275e059a45958eef51c3ab068d
Samanehta <samanehtassalot@gmail.com>
2025-04-03 01:15:58 +0330

    Add files via upload

---
+++ سوال 7 دریافت عدد و عملیات/سوال 7 دریافت عدد و عملیات/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double a, b;
+        char c;
+        Console.WriteLine("ple enter number 1 : ");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("ple enter number 2 : ");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter your operation : ");
+        c = char.Parse(Console.ReadLine());
+        if (c == '*')
+            Console.WriteLine("result = " + a * b);
+        else if (c == '/')
+            Console.WriteLine("result = " + a / b);
+        else if (c == '+')
+            Console.WriteLine("result = " + (a + b));
+        else if (c == '-')
+            Console.WriteLine("result = " + (a - b));
+        Console.ReadKey();
+    }
+}
---
+++ سوال 8 مضارب 5 بین 2 عدد/سوال 8 مضارب 5 بین 2 عدد/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a, b;
+        Console.WriteLine("please enter number 1 : ");
+        a = int.Parse(Console.ReadLine());
+        Console.WriteLine("please enter number 2 : ");
+        b = int.Parse(Console.ReadLine());
+        for (int i = a + 1; i <= b - 1; i++)
+            if (i % 5 == 0)
+                Console.WriteLine("javab : " + i);
+        Console.ReadKey();
+    }
+}
